09c22702e8f7b035c07e1f579d576f17cf4309e9
Cozy-ctrl <63079225+Cozy-ctrl@users.noreply.github.com>
2025-05-10 13:37:53 -0700

    Fix SDK import in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { VapiClient } = require('@vapi-ai/server-sdk');
+const { VapiClient } = require('./dist');
 
 // Initialize the Vapi client with your API token
 const vapiClient = new VapiClient({
